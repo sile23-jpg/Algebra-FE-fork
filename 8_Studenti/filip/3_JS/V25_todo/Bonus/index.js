d328bbd8575f9bb4a6ffb0811b82a50bf333b357
sile23-jpg <fmarinovic3@gmail.com>
2025-09-02 21:29:19 +0200

    v27
    a

---
+++ 8_Studenti/filip/3_JS/V25_todo/Bonus/index.js
@@ -0,0 +1,16 @@
+// import { drugi } from "./srcipt.js";
+
+import main from "./src/script.js";
+
+document.addEventListener("DOMContetLoaded", main);
+
+var prvi = 10;
+
+console.log(prvi);
+console.log(drugi);
+
+// var prvi = 10;
+
+// (function () {
+//   var drugi = 20;
+// })();
