a0a5bfa3d1bafdee398691eb8569e884f6b51a36
sile23-jpg <fmarinovic3@gmail.com>
2025-09-11 18:06:01 +0200

    bonus

--- 8_Studenti/filip/9_Bonus/digirn/html_css_js/index.js
+++ 8_Studenti/filip/9_Bonus/digirn/html_css_js/index.js
@@ -1,9 +1,16 @@
-import { toggleNav } from "./js/shared/functions";
+import { toggleNav } from "./js/shared/functions.js";
 
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
   let hamburgerEl = document.getElementById("hamburger-icon");
+  let asideEl = document.getElementsByTagName("aside")[0];
 
-  hamburgerEl.addEventListener("click", toggleNav(hamburgerEl));
+  document.addEventListener("click", (event) => {
+    const asideElDisplay = window.getComputedStyle(asideEl).display;
+    if (!event.target.closest("div") && asideElDisplay === "block")
+      toggleNav(hamburgerEl, asideEl);
+  });
+
+  hamburgerEl.addEventListener("click", () => toggleNav(hamburgerEl, asideEl));
 }
